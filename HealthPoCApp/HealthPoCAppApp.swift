725699393aa9274f96ccf2a102b1fc5004b4cc85
Miroslav Durkovic <mirec.durkovic@gmail.com>
2023-07-16 12:09:05 +0200

    Initial Commit

---
+++ HealthPoCApp/HealthPoCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthPoCAppApp.swift
+//  HealthPoCApp
+//
+//  Created by Miroslav Durkovic on 16/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthPoCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
